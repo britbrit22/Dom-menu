bcd10421e94b1a2705536b9475db783d0c367110
britbrit22 <https://github.com/britbrit22/Hidden-Foods-Games.git>
2023-03-19 11:01:43 -0700

    complete

--- js/script.js
+++ js/script.js
@@ -1,41 +1,11 @@
 
-var menuLinks = [
+const menuLinks = [
     {text: 'about', href: '/about'},
     {text: 'catalog', href: '/catalog'},
     {text: 'orders', href: '/orders'},
     {text: 'account', href: '/account'},
   ];
 
-  var a = document.createElement('a');
-      var linkText = document.createTextNode('about');
-      a.appendChild(linkText);
-      a.text = 'about';
-      a.href = '/about';
-      document.body.appendChild(a);
-
-  var a = document.createElement('a');
-      var linkText = document.createTextNode('catalog');
-      a.appendChild(linkText);
-      a.text = 'catalog';
-      a.href = '/catalog';
-      document.body.appendChild(a);
-
-  var a = document.createElement('a');
-      var linkText = document.createTextNode('orders');
-      a.appendChild(linkText);
-      a.text = 'orders';
-      a.href = '/orders';
-      document.body.appendChild(a);
-
-  var a = document.createElement('a');
-      var linkText = document.createTextNode('account');
-      a.appendChild(linkText);
-      a.text = 'account';
-      a.href = '/account';
-      document.body.appendChild(a);            
-
-
-  
 const mainEl = document.querySelector('main');
 console.log(mainEl)
 
@@ -50,6 +20,38 @@ console.log(topMenuEl)
 
 topMenuEl.style.height = '100%';
 
-topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 
-topMenuEl.classList.add('flex-around')
+topMenuEl.classList.add('flex-around');
+
+  var a = document.createElement('a');
+      var linkText = document.createTextNode('about');
+      a.appendChild(linkText);
+      a.text = 'about';
+      a.href = '/about';
+      topMenuEl.appendChild(a);
+
+  var a = document.createElement('a');
+      var linkText = document.createTextNode('catalog');
+      a.appendChild(linkText);
+      a.text = 'catalog';
+      a.href = '/catalog';
+      topMenuEl.appendChild(a);
+
+  var a = document.createElement('a');
+      var linkText = document.createTextNode('orders');
+      a.appendChild(linkText);
+      a.text = 'orders';
+      a.href = '/orders';
+      topMenuEl.appendChild(a);
+
+  var a = document.createElement('a');
+      var linkText = document.createTextNode('account');
+      a.appendChild(linkText);
+      a.text = 'account';
+      a.href = '/account';
+      topMenuEl.appendChild(a);            
+
+
+  
+
